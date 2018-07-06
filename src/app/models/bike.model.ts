c7bd0c7dcba833a415af2a1511be821a81eec070
Walter Leinert <walter.leinert@hp.com>
2018-07-06 16:19:46 +0200

    added model, api methods

---
+++ src/app/models/bike.model.ts
@@ -0,0 +1,11 @@
+export interface Bike {
+    id: number;
+    name: string;
+    email: string;
+    phone: string;
+    model: string;
+    serialNumber?: any;
+    purchasePrice?: number;
+    purchaseDate: string;
+    contact: boolean;
+}
--- src/app/services/bike.service.ts
+++ src/app/services/bike.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Bike } from '../models/bike.model';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable()
 export class BikeService {
@@ -7,8 +13,16 @@ export class BikeService {
   constructor(private http: HttpClient) {
   }
 
-  public getBikes() {
-    return this.http.get('/server/api/v1/bikes');
+  public getBikes(): Observable<Bike[]> {
+    return this.http.get<Bike[]>('/server/api/v1/bikes');
+  }
+
+  public getBike(id: number): Observable<Bike> {
+    return this.http.get<Bike>('/server/api/v1/bikes' + id);
+  }
+
+  public create(bike: Bike): Observable<Bike> {
+    return this.http.post<Bike>('/server/api/v1/bikes', bike, httpOptions);
   }
 
 }
